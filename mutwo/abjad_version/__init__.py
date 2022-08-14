96915178b641fdb3bed0417af90d650dac0f5fcc
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-08-14 14:57:01 +0200

    fixup! Expose version to documentation

--- mutwo/abjad_version/__init__.py
+++ mutwo/abjad_version/__init__.py
@@ -1,6 +1,6 @@
 MAJOR, MINOR, PATCH = 0, 11, 1
 
-__version__ = f"{MAJOR}.{MINOR}.{PATCH}"
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 """The version of the package ``mutwo.abjad``."""
 
 del MAJOR, MINOR, PATCH
