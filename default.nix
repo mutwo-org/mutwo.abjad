8d51930bd811796938238169884596c0abb17bcd
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-19 16:33:24 +0100

    Update mutwo.music in nix

--- default.nix
+++ default.nix
@@ -6,10 +6,10 @@ let
   mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/6a5f2ca81e6e4e06b7ad4d6f46b0361ceaca4ea1.tar.gz";
+  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/10c4f7f9fd4ec5d051b73dbffe2edd0628f9752f.tar.gz";
   mutwo-music = import (mutwo-music-archive + "/default.nix");
 
-  mutwo-ekmelily-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.ekmelily/archive/e466119683d076b60c7e65b2570fdefc8382fe3a.tar.gz";
+  mutwo-ekmelily-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.ekmelily/archive/b84d86d026745ce08dbcf49aa96fe45056ffe4c1.tar.gz";
   mutwo-ekmelily = import (mutwo-ekmelily-archive + "/default.nix");
 
   quicktions = pkgs.python310Packages.buildPythonPackage rec {
