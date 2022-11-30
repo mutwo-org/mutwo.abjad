8c735d1b41b4ba214c7bf4c3b1529a983c533380
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-11-30 19:19:57 +0100

    Update mutwo.core in nix to v.1.2.0

--- default.nix
+++ default.nix
@@ -3,13 +3,7 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
-  mutwo-core = import (mutwo-core-archive + "/default.nix");
-
-  mutwo-music-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.music/archive/10c4f7f9fd4ec5d051b73dbffe2edd0628f9752f.tar.gz";
-  mutwo-music = import (mutwo-music-archive + "/default.nix");
-
-  mutwo-ekmelily-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.ekmelily/archive/b84d86d026745ce08dbcf49aa96fe45056ffe4c1.tar.gz";
+  mutwo-ekmelily-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.ekmelily/archive/591dd6095ed09fb5be1ed219fb62bc830c89734a.tar.gz";
   mutwo-ekmelily = import (mutwo-ekmelily-archive + "/default.nix");
 
   quicktions = pkgs.python310Packages.buildPythonPackage rec {
@@ -142,8 +136,6 @@ in
     propagatedBuildInputs = [
       abjad
       abjad-ext-nauert
-      mutwo-core
-      mutwo-music
       mutwo-ekmelily
       lilypond-with-fonts
     ];
