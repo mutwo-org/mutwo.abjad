cc149499ec098ec2c548509128192a99ee7dc2ed
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-06-08 18:38:12 +0200

    nix: Use mutwo-nix, but with local src

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 let
-
-  mutwo-nix-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/main.tar.gz";
-  mutwo-abjad = import (mutwo-nix-archive + "/mutwo.abjad/default.nix");
-
-
+  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
+  mutwo-abjad = import (sourcesTarball + "/mutwo.abjad/default.nix") {};
+  mutwo-abjad-local = mutwo-abjad.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
-
-  mutwo-abjad
+  mutwo-abjad-local
